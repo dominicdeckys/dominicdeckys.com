e9e8e152605c4dd5bf7d0b0b1e47bf68a620bfea
Unknown <dominic.deckys@colorado.edu>
2018-07-28 17:08:59 -0600

    Add login and sql functionality to new template

---
+++ develop/login.php
@@ -0,0 +1,85 @@
+<?php
+   include("config.php");
+   session_start();
+   $passwordClass = "form-control";
+   
+   if($_SERVER["REQUEST_METHOD"] == "POST") {
+      // username and password sent from form 
+      
+      $myusername = mysqli_real_escape_string($db,$_POST['username']);
+      $mypassword = mysqli_real_escape_string($db,$_POST['password']); 
+      
+      $sql = "SELECT id FROM admin WHERE username = '$myusername' and passcode = '$mypassword'";
+      $result = mysqli_query($db,$sql);
+      $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
+      $active = $row['active'];
+      
+      $count = mysqli_num_rows($result);
+      
+      // If result matched $myusername and $mypassword, table row must be 1 row
+		
+      if($count == 1) {
+         // session_register("myusername");
+         $_SESSION['login_user'] = $myusername;
+         
+         header("location: welcome.php");
+      }else {
+         $error = "Your Login Name or Password is invalid";
+         $passwordClass = "form-control is-invalid";
+      }
+   }
+?>
+<html>
+   
+   <head>
+      <title>Login Page</title>
+      
+      <link rel="stylesheet" href="css/bootstrap.css" media="screen">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+      
+   </head>
+   
+   <body bgcolor = "#FFFFFF">
+
+<div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+      <div class="container">
+        <a href="" class="navbar-brand">Dom's Social Network</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="">Login</a>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+    <div>
+    <p>
+      <br /><br /><br />
+    </p>
+    </div>
+
+    <div align = "center">
+       <div align = "left" class="card border-secondary mb-3" style="max-width: 20rem;">
+          <div class="card-header">Login</div>
+          <div class="card-body">
+            <form action = "" method = "post">
+               <label>UserName  :</label><input type = "text" name = "username" class = "form-control"/><br /><br />
+               <label>Password  :</label><input type = "password" name = "password" class="<?php echo $passwordClass; ?>" /><br/><br />
+               <div style="text-align: center;">
+                  <input class="btn btn-secondary" type = "submit" value = " Submit "/><br />
+                  <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
+               </div>
+            </form>
+          </div>
+        </div>
+     </div>
+
+   </body>
+</html>
